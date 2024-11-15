feacb3afe37a80d1c544b01e56767fd3586e3d47
Your Name <you@example.com>
2024-11-15 09:34:51 +0100

    exo2

---
+++ exo2/exo2.js
@@ -0,0 +1,9 @@
+var MavariableString = "TOTO";
+
+var MavariableNumerique = 100;
+
+var Mavariabletableau = ['apple' , 'banana'];
+
+var Mavariable0bjet = window
+
+alert(MavariableString + MavariableNumerique + Mavariabletableau[0] + Mavariable0bjet.location.pathname);
