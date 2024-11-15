09ccdb161bb369e5b4acde9040b366cfefdc448f
Your Name <you@example.com>
2024-11-15 09:46:12 +0100

    exo3

---
+++ exo3/exo3.js
@@ -0,0 +1,9 @@
+var MavariableString = "TOTO";
+
+var MavariableNumerique = prompt("entre un nombre",100);
+
+if (MavariableString =="TOTO" && MavariableNumerique !=200 || MavariableNumerique <=100){
+    alert("je suis le cas 1");
+}else{
+    alert("je suis le cas 2");
+}
